ae53a101ebf138cbad4a98767ce9797618020f22
seushin <seushin@student.42seoul.kr>
2021-06-30 15:50:13 +0900

    wp-cli 설치 및 설정

--- srcs/wordpress/tools/docker-entrypoint.sh
+++ srcs/wordpress/tools/docker-entrypoint.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
-if [ -e /tmp/wp-config.php ]; then
+if [ ! -e /var/www/wordpress/wp-config.php ]; then
 	echo "** creating wp-config file"
-	envsubst '$MYSQL_DATABASE $MYSQL_USER $MYSQL_PASSWORD' < /tmp/wp-config.php > /var/www/wordpress/wp-config.php
-	rm -f /tmp/wp-config.php
+	# envsubst '$MYSQL_DATABASE $MYSQL_USER $MYSQL_PASSWORD' < /tmp/wp-config.php > /var/www/wordpress/wp-config.php
+	wp config create \
+		--dbname=${MYSQL_DATABASE} \
+		--dbuser=${MYSQL_USER} \
+		--dbpass=${MYSQL_PASSWORD} \
+		--dbhost=mariadb:3306
+
+	echo "** wp core install.."
+	wp core install \
+		--url=${DOMAIN_NAME} \
+		--title=${COMPOSE_PROJECT_NAME} \
+		--admin_user=${WP_ADMIN} \
+		--admin_password=${WP_ADMIN_PASSWORD} \
+		--admin_email=${WP_ADMIN_EMAIL} \
+		--skip-email
+
+	echo "** wp user create ${WP_USER}.."
+	wp user create ${WP_USER} ${WP_USER_EMAIL} \
+		--user_pass=${WP_USER_PASSWORD} \
+		--role=author
+	# echo "** rm /tmp/wp-config.php"
+	# rm -f /tmp/wp-config.php
 fi
 
+echo "** run $@"
 exec "$@"
